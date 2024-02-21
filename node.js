ebc38f9a1ead21c372ae64b0c2ae3dce13a1d88f
The good guy? <80371931+NPSBlackocean@users.noreply.github.com>
2024-02-21 00:43:47 -0300

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,16 @@
-import express
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; // Define a porta do servidor
+
+// Middleware para tratar requisições JSON
+app.use(express.json());
+
+// Rota de exemplo
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
+// Inicie o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
